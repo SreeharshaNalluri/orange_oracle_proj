7f8cbf6f978a6dc183358e4769b2615d1ab410f7
SreeharshaNalluri <harshanalluri.123@gmail.com>
2025-04-25 18:17:25 -0700

    Added CSS variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#1F2937", // Custom primary color (gray-800)
-        secondary: "#111827", // Custom secondary color (gray-900)
-        accent: "#D1D5DB", // Custom accent color (gray-300)
-        background: "#F3F4F6", // Custom background color (gray-100)
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"], // Custom font family
-      },
-      spacing: {
-        128: "32rem", // Custom spacing
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        surface: "var(--surface)",
+        accent: "var(--accent)",
+        "text-secondary": "var(--text-secondary)",
       },
     },
   },
